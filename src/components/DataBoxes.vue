731003fe77b232d278c0fa07531f6a89da3af00e
Mihael Zsivics <zsivics.mihael@gmail.com>
2021-07-04 13:51:49 +0200

    Created vue-covid19-tracker app following tutorial. Link to Tutorial: https://www.youtube.com/watch?v=m-MAIpnH9ag

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="grid md:grid-cols-2 gap-4">
+        <!-- Box 1 -->
+        <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{ formatNumber(stats.NewConfirmed) }}
+            </div>
+
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                {{ formatNumber(stats.TotalConfirmed) }}
+            </div>
+        </div>
+
+        <!-- Box 2 -->
+        <div class="shadow-md bg-blue-200 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
+
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{ formatNumber(stats.NewDeaths) }}
+            </div>
+
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                {{ formatNumber(stats.TotalDeaths) }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DataBoxes",
+    props: ["stats"],
+    methods: {
+        formatNumber(number) {
+            return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        }
+    }
+}
+</script>
